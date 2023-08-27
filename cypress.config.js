c92809cb0dea353acef249db05ca366eb6538883
Dennis Whalen <dwwhalen@gmail.com>
2023-08-27 16:03:28 -0400

    added a cypress test that is able to select data from the database

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,70 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        logIt(message) {
+          console.log(message)
+
+          return null
+        },
+      }),
+        on('task', {
+          queryDatabase(query) {
+            console.log(query)
+            var config = {
+              "server": "localhost",
+              "authentication": {
+                "type": "default",
+                "options": {
+                  "userName": process.env.CYPRESS_DB_ID,
+                  "password": process.env.CYPRESS_DB_PW
+                }
+              },
+              "options": {
+                "port": 1433,
+                "database": "testdata",
+                "trustServerCertificate": true
+              }
+            }
+
+            const Request = require('tedious').Request;
+            const Connection = require('tedious').Connection;
+            const connection = new Connection(config);
+
+            connection.on('connect', (err) => {
+              if (err) {
+                console.log('Connection Failed');
+                throw err;
+              } else {
+                console.log('Connected!');
+                return executeSqlStatement(query);
+              }
+            });
+            connection.connect();
+
+            function executeSqlStatement(theQuery) {
+              const request = new Request(theQuery, (err, rowCount) => {
+                if (err) {
+                  throw err;
+                }
+                console.log('DONE!');
+                connection.close();
+              });
+              // Emits a 'DoneInProc' event when completed.
+              request.on('row', (columns) => {
+                columns.forEach((column) => {
+                  if (column.value === null) {
+                    console.log('NULL');
+                  } else {
+                    console.log(column.value);
+                  }
+                });
+              });
+              connection.execSql(request);
+            }
+            return null
+          },
+        })
     },
   },
 });
---
+++ cypress/e2e/1-getting-started/database.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('example database access', () => {
+  beforeEach(() => {
+    cy.log("IN BEFORE EACH");
+  })
+
+  it('access database', () => {
+    cy.task('logIt', 'trying the access database test')
+    cy.task('queryDatabase', 'select last_name, first_name from member')
+  })
+})
+
