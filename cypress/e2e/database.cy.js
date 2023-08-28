9a9f74f253742aa096d8ac7ff1119f076b737ea9
Dennis Whalen <dwwhalen@gmail.com>
2023-08-28 11:01:01 -0400

    tweaked the test a little to include an assert of the SQL results

--- cypress/e2e/database.cy.js
+++ cypress/e2e/database.cy.js
@@ -4,6 +4,8 @@ describe('example database access', () => {
 
   it.only('access mssql database', () => {
     cy.task('mssqlQuery', 'select last_name, first_name from member order by first_name asc')
-      .then((theResponse) => { cy.log("THE RESPONSE RETURNED TO THE TEST: " + theResponse.recordset[0].last_name) })
+      .then((theResponse) => { 
+        expect(theResponse.recordset[0].last_name).to.equal('whalen')
+        cy.log("THE RESPONSE RETURNED TO THE TEST: " + theResponse.recordset[0].last_name) })
   })
 })
