9884de62486a9c34510d72d085ddf17487a011e2
Dennis Whalen <dwwhalen@gmail.com>
2023-08-28 10:51:43 -0400

    prettied up some logging

--- cypress.config.js
+++ cypress.config.js
@@ -12,9 +12,10 @@ module.exports = defineConfig({
       }),
       on('task', {
         async mssqlQuery(query) {
-          console.log("query input to mssqlQuery: task" + query)
+          console.log("query INPUT to mssqlQuery task: " + query)
           await sql.connect(`Server=localhost,1433;Database=testdata;User Id=${process.env.CYPRESS_DB_ID};Password=${process.env.CYPRESS_DB_PW};Encrypt=true;trustServerCertificate=true`)
           const queryResult = await sql.query(query)
+          console.log("query OUTPUT from mssqlQuery task: ");
           console.log(queryResult);
           return queryResult
         },
